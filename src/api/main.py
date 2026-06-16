4bfe154d35692cb339e57ba037a63ea4a56b3443
Joao <joaopedrosouzarodri@gmail.com>
2026-06-16 01:20:51 -0300

    feat(api): add /health and /mlflow/experimentos endpoints

--- src/api/main.py
+++ src/api/main.py
@@ -14,7 +14,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost", "http://localhost:3000", "http://localhost:8080"],
+    allow_origins=["http://localhost", "http://localhost:3000", "http://localhost:8080", "http://127.0.0.1:8000"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
